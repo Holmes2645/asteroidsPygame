9218ff68011246af35d6b4058ac185f9b0487f74
Holmesith <cheetochip99@gmail.com>
2025-03-27 14:50:26 -0600

    Added renderer, display terminal and ignore file

--- main.py
+++ main.py
@@ -9,6 +9,21 @@ def main():
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
 
+    pygame.init()
+    screen = pygame.display.set_mode((SCREEN_WIDTH,SCREEN_HEIGHT))
+    color = pygame.Color(0,0,0)
+
+    while True:
+
+        #Event handler: Quit Game 
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+
+        #Fills screen with black
+        screen.fill(color)
+        pygame.display.flip()
+
 
 if __name__ == "__main__":
     main()
